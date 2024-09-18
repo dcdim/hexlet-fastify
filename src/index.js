ab0b88b89441049287fa5d19cfb2366e0a690930
Дмитрий Каширин <dcdim@mail.ru>
2024-09-18 22:14:08 +0300

    http-session

--- src/index.js
+++ src/index.js
@@ -3,8 +3,36 @@ import fastify from 'fastify';
 const app = fastify();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// app.get('/users', (req, res) => {
+//   res.send('GET /users');
+// });
+
+// app.post('/users', (req, res) => {
+//   res.send('POST /users');
+// });
+
+// const state = {
+//   users: [
+//     {
+//       id: 1,
+//       name: 'Alice',
+//     },
+//     {
+//       id: 2,
+//       name: 'Bob',
+//     },
+//   ],
+// };
+
+app.get('/hello', (req, res) => {
+  const name = req.query.name;
+  // const { id } = req.query;
+  // const user = state.users.find(user => user.id === parseInt(id));
+  if (!name) {
+    res.send('Hello, World!');
+  } else {
+    res.send(`Hello, ${name}!`);
+  }
 });
 
 app.listen({ port }, () => {
