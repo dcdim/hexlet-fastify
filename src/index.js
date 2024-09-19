056287fdac367f7f916eb02d01f357751dd88884
Дмитрий Каширин <dcdim@mail.ru>
2024-09-19 21:45:30 +0300

    dynamic-routes

--- src/index.js
+++ src/index.js
@@ -3,36 +3,17 @@ import fastify from 'fastify';
 const app = fastify();
 const port = 3000;
 
-// app.get('/users', (req, res) => {
-//   res.send('GET /users');
+// app.get('/hello', (req, res) => {
+//   const name = req.query.name;
+//   if (!name) {
+//     res.send('Hello, World!');
+//   } else {
+//     res.send(`Hello, ${name}!`);
+//   }
 // });
 
-// app.post('/users', (req, res) => {
-//   res.send('POST /users');
-// });
-
-// const state = {
-//   users: [
-//     {
-//       id: 1,
-//       name: 'Alice',
-//     },
-//     {
-//       id: 2,
-//       name: 'Bob',
-//     },
-//   ],
-// };
-
-app.get('/hello', (req, res) => {
-  const name = req.query.name;
-  // const { id } = req.query;
-  // const user = state.users.find(user => user.id === parseInt(id));
-  if (!name) {
-    res.send('Hello, World!');
-  } else {
-    res.send(`Hello, ${name}!`);
-  }
+app.get('/users/:id/post/:postId', (req, res) => {
+  res.send(`User ID: ${req.params.id}; Post ID: ${req.params.postId}`);
 });
 
 app.listen({ port }, () => {
